02d6df0239b199b7ad339116c74856cc2ab7077e
Today <76273733+today2098@users.noreply.github.com>
2024-06-19 16:32:37 +0900

    [add] Create a test file of "sum.go"

---
+++ lib/examples/sum_test.go
@@ -0,0 +1,24 @@
+package examples
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Sum(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("Sum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
